cfb8468fbeb157c27f53ea40566a679815e2e8a8
nmemme <nmemme@bu.edu>
2013-11-29 09:55:54 -0500

    Upload button

--- CAGE/CAGE/FullscreenActivity.java
+++ CAGE/CAGE/FullscreenActivity.java
@@ -6,15 +6,18 @@ import android.R.id;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Picture;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -63,8 +66,8 @@ public class FullscreenActivity extends Activity {
 
         setContentView(R.layout.activity_fullscreen);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
+        final View controlsView = findViewById(R.id.Capture);
+        final View contentView = findViewById(R.id.Capture);
 
         // Set up an instance of SystemUiHider to control the system UI for
         // this activity.
@@ -109,14 +112,16 @@ public class FullscreenActivity extends Activity {
                 });
 
         // Set up the user interaction to manually show or hide the system UI.
-        Button captureButton = (Button) findViewById(R.id.capture);
+        Button captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(
             new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v) 
+                {
                     // get an image from the camera
                 	
-                    mCamera.takePicture(null, null, mPicture);
+                    //mCamera.takePicture(null, null, mPicture);
+                	System.out.println("Click");
                 }
             }
         );
@@ -134,7 +139,7 @@ public class FullscreenActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.capture).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.button_capture).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -189,5 +194,78 @@ public class FullscreenActivity extends Activity {
 		mImageView.setImageBitmap(mImageBitmap);
     }
     
-    
+    public class BrowsePicture extends Activity {
+
+        //YOU CAN EDIT THIS TO WHATEVER YOU WANT
+        private static final int SELECT_PICTURE = 1;
+
+        private String selectedImagePath;
+        //ADDED
+        private String filemanagerstring;
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_fullscreen);
+
+            ((Button) findViewById(R.id.upload))
+            .setOnClickListener(new OnClickListener() {
+
+                public void onClick(View arg0) {
+
+                    // in onCreate or any event where your want the user to
+                    // select a file
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent,
+                            "Select Picture"), SELECT_PICTURE);
+                }
+            });
+        }
+
+        //UPDATED
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode == RESULT_OK) {
+                if (requestCode == SELECT_PICTURE) {
+                    Uri selectedImageUri = data.getData();
+
+                    //OI FILE Manager
+                    filemanagerstring = selectedImageUri.getPath();
+
+                    //MEDIA GALLERY
+                    selectedImagePath = getPath(selectedImageUri);
+
+                    //DEBUG PURPOSE - you can delete this if you want
+                    if(selectedImagePath!=null)
+                        System.out.println(selectedImagePath);
+                    else System.out.println("selectedImagePath is null");
+                    if(filemanagerstring!=null)
+                        System.out.println(filemanagerstring);
+                    else System.out.println("filemanagerstring is null");
+
+                    //NOW WE HAVE OUR WANTED STRING
+                    if(selectedImagePath!=null)
+                        System.out.println("selectedImagePath is the right one for you!");
+                    else
+                        System.out.println("filemanagerstring is the right one for you!");
+                }
+            }
+        }
+
+        //UPDATED!
+        public String getPath(Uri uri) {
+            String[] projection = { MediaStore.Images.Media.DATA };
+            Cursor cursor = managedQuery(uri, projection, null, null, null);
+            if(cursor!=null)
+            {
+                //HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+                //THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+                int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            }
+            else return null;
+        }
+    }
 }
