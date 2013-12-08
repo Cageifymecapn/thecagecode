e913db189ec2922f82fdf2fbc87a9a42a4f05830
piandao <geojohn@bu.edu>
2013-12-08 11:18:55 -0500

    SPLASH SCREEN
    Includes update to manifest and splash.java

---
+++ TheCagifier/src/com/example/thecagifier/splash.java
@@ -0,0 +1,46 @@
+package com.example.thecagifier;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class splash extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setContentView(R.layout.splash);
+		
+		IntentLauncher launcher = new IntentLauncher();
+		launcher.start();
+	}
+		
+	private class IntentLauncher extends Thread{
+			public void run(){
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		Intent intent = new Intent(splash.this, MainScreen.class);
+		splash.this.startActivity(intent);
+		splash.this.finish();
+			}
+		};
+		
+		
+	
+@Override
+protected void onPause() {
+	super.onPause();
+}
+
+}
