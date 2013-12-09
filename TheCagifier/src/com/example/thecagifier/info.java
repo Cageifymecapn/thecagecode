b4308eac970bd0af0b2dbcfa852195f4298ebbbb
etcarson <etc2913@gmail.com>
2013-12-09 07:42:38 -0500

    Monday Morning Change.

--- TheCagifier/src/com/example/thecagifier/info.java
+++ TheCagifier/src/com/example/thecagifier/info.java
@@ -1,28 +1,26 @@
 package com.example.thecagifier;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
-
 import com.example.thecagifier.R;
 
-
-public class info extends Activity  {
-
-    ImageButton Undo=(ImageButton) findViewById(R.id.imageButton1);
-    public void OnCreate (Bundle savedInstanceState){
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.info);
-    
-
-    Undo.setOnClickListener(new OnClickListener() 
-    {
-		@Override
-		public void onClick(View arg0) {
-			setContentView(R.layout.mainscreen);
-		}
-    });
-}}
+public class info extends Activity{
+	
+@Override
+    public void onCreate (Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+		
+    	ImageButton back = (ImageButton) findViewById(R.id.back);        
+        back.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		setContentView(R.layout.info);
+        		startActivity(new Intent(info.this, MainScreen.class));
+        	}
+        });
+	}
+}
