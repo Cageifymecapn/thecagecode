4f2b466095ff1560dc3d9b2d3da4701b5c66150f
piandao <geojohn@bu.edu>
2013-12-08 23:30:49 -0500

    It works
    Need to rotate image, flip camera, and a few other things

--- TheCagifier/src/com/example/thecagifier/MainScreen.java
+++ TheCagifier/src/com/example/thecagifier/MainScreen.java
@@ -7,10 +7,15 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.content.Intent;
 
 import android.database.Cursor;
@@ -29,20 +34,24 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainScreen extends Activity implements SurfaceHolder.Callback {
-TextView testView;
-
-Camera camera;
-SurfaceView surfaceView;
-SurfaceHolder surfaceHolder;
-PictureCallback rawCallback;
-ShutterCallback shutterCallback;
-PictureCallback jpegCallback;
-
-private final String tag = "mainscreen";
+	
+	TextView testView;
+	
+	Camera camera;
+	SurfaceView surfaceView;
+	SurfaceHolder surfaceHolder;
+	PictureCallback rawCallback;
+	ShutterCallback shutterCallback;
+	Camera.PictureCallback jpegCallback;
+	PictureCallback postviewCallback;
+	
+	private final String tag = "mainscreen";
+	
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
         
@@ -53,84 +62,91 @@ private final String tag = "mainscreen";
         //pushed camera frames to screen
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        //Makes camera preview automaticallly come up on surface view
+        //Makes camera preview automatically come up on surface view
         surfaceView.setVisibility(0);
         
-    ImageButton takephoto = (ImageButton) findViewById(R.id.takepicture);
-    ImageButton openGallery = (ImageButton) findViewById(R.id.gallery);
-    ImageButton info = (ImageButton) findViewById(R.id.info);
+        ImageButton takephoto = (ImageButton) findViewById(R.id.takepicture);
+        ImageButton openGallery = (ImageButton) findViewById(R.id.gallery);
+        ImageButton info = (ImageButton) findViewById(R.id.info);
+        
     
-    
-    //Leads to Info Screen
-	info.setOnClickListener(new OnClickListener() 
-    {
-		@Override
-		public void onClick(View arg0) {
-			setContentView(R.layout.info);
-		}
-    });
-    
-    
-    //Response to the takepicture button being pushed
-    takephoto.setOnClickListener(new OnClickListener() 
-    {
-		@Override
-		public void onClick(View arg0) {
-	        takePicture();
-	        setContentView(R.layout.editor);
-		}
-    });
-    
-    //Everett: Opens the gallery when Gallery button is pressed. 
-    //Got help for the Intent from http://stackoverflow.com/questions/18416122/open-gallery-app-in-android
-    openGallery.setOnClickListener(new OnClickListener()
-    {
-    	@Override
-    	public void onClick(View arg0) {
-    		//Android has something to open the gallery already
-    		Intent galleryIntent = new Intent(Intent.ACTION_VIEW, 
-    				Uri.parse("content://media/internal/images/media"));
-    		startActivity(galleryIntent);
-    		startActivityForResult(galleryIntent, 1);
-    	}
-    });
-}
+	    //Leads to Info Screen
+		info.setOnClickListener(new OnClickListener() 
+	    {
+			@Override
+			public void onClick(View arg0) {
+				setContentView(R.layout.info);
+			}
+	    });
+		  
+	    //Response to the takepicture button being pushed
+	    takephoto.setOnClickListener(new OnClickListener() 
+	    {
+			@Override
+			public void onClick(View arg0) {
+		        camera.takePicture(null, null, jpegCallback);
+//		        Bitmap pictureTaken = BitmapFactory.decodeFile("content://media/internal/images/test1.jpg");
+//	            ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+//	            editorPicture.setImageBitmap(pictureTaken);  
+//		        setContentView(R.layout.editor);
+			}
+	    });
+	    
+	    //Everett: Opens the gallery when Gallery button is pressed. 
+	    //Got help for the Intent from http://stackoverflow.com/questions/18416122/open-gallery-app-in-android
+	    openGallery.setOnClickListener(new OnClickListener()
+	    {
+	    	@Override
+	    	public void onClick(View arg0) {
+	    		//Android has something to open the gallery already
+	    		Intent galleryIntent = new Intent(Intent.ACTION_PICK, 
+	    				Uri.parse("content://media/internal/images/media"));
+	    		startActivityForResult(galleryIntent, 1);
+	    	}
+	    });
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {    
     	if (requestCode==0)
     	{
-    		takePicture();
-    		}
-    	
-//    	if(requestCode == 0)
-//    	{
-//    		Bitmap theImage = (Bitmap) data.getExtras().get("data"); // Austin : Set Background to the photo
-//    		Picture.setImageBitmap(theImage);
-//    		try {
-//				galleryAddPic(data);
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//    		FacialRecognition(theImage);
-//    	}
-    	
-    	//Everett: The request code for opening the gallery and selecting an image
-    	if(requestCode == 1)
-    	{
-            setContentView(R.layout.editor);
-    	    Uri selectedImageUri = data.getData();
+//    		Bundle extras = data.getExtras();
+//    		Bitmap pictureTakenBitmap = (Bitmap) extras.get("data");
+//    		int width = pictureTakenBitmap.getWidth();
+//    		int height = pictureTakenBitmap.getHeight();
+//    		ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+//            editorPicture.setImageBitmap(pictureTakenBitmap);
+    		setContentView(R.layout.editor);
+    		Uri selectedImageUri = data.getData();
             String selectedImagePath = getPath(selectedImageUri);
             //sets theImage to the one of the selected path, and then sets the 
             //background Picture to that theImage
             Bitmap selectedImage = BitmapFactory.decodeFile(selectedImagePath);
             ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
-            editorPicture.setImageBitmap(selectedImage);  
-    	}   	
+            editorPicture.setImageBitmap(selectedImage);
+    	}
+    	
+    	//Everett: The request code for opening the gallery and selecting an image
+    	if(requestCode == 1)
+    	{
+    		setContentView(R.layout.editor);
+    		Uri selectedImageUri = data.getData();
+            String selectedImagePath = getPath(selectedImageUri);
+            
+            //sets theImage to the one of the selected path, and then sets the 
+            //background Picture to that theImage
+            Bitmap selectedImage = BitmapFactory.decodeFile(selectedImagePath);
+            ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+            editorPicture.setImageBitmap(selectedImage);                          
+    	}
+    	
+    	if(requestCode == 2)
+    	{
+    		//This adds the photo to the gallery
+    	}
     };
-    
+
     private void galleryAddPic(Intent data) throws IOException{
 	//Create an image file name
     String timeStamp = 
@@ -154,78 +170,114 @@ private final String tag = "mainscreen";
 	String fileString = file.toString();
     Uri contentUri = Uri.fromFile(file);
     Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, contentUri);
-    sendBroadcast(mediaScanIntent);
+    startActivityForResult(mediaScanIntent, 2);
 
 //  Bitmap theImage = (Bitmap) data.getExtras().get("data");
 //  MediaStore.Images.Media.insertImage(getContentResolver(), theImage, file.getName() , "Made by Cagifier");
 
   }
-    
-    
-    @Override
+
+	@Override
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
 		// TODO Auto-generated method stub
+//		shutterCallback = new ShutterCallback() {
+//			public void onShutter() {
+//				// TODO Do something when the shutter closes.
+//	        }
+//		};
 		
-		shutterCallback = new ShutterCallback() {
-	        public void onShutter() {
-	          // TODO Do something when the shutter closes.
-	        }
-	      };
+//		rawCallback = new PictureCallback() {
+//	        public void onPictureTaken(byte[] data, Camera camera) {
+////	        	Bitmap pictureTaken = BitmapFactory.decodeByteArray(data, 0, data.length);
+////	 	        ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+////	 	        editorPicture.setImageBitmap(pictureTaken);
+////	        	//Do something with the image RAW data.
+////	        	FileOutputStream outStream = null;
+////	        	try {
+////	        		String path = Environment.getExternalStorageDirectory() +
+////	        				"/test.jpg";
+////	        		outStream = new FileOutputStream(path);
+////	        		outStream.write(data);
+////	        		outStream.close();
+////	        	} catch (FileNotFoundException e) {
+////	        		Log.e(tag, "File Note Found", e);
+////	        	} catch (IOException e) {
+////	        		Log.e(tag, "IO Exception", e);
+////	        	}
+//	        }
+//		};
+	     jpegCallback = new PictureCallback() {
+	    	 public void onPictureTaken(byte[] data, Camera camera) {
+	    		 //Save the image JPEG data to the SD card
+	    		 FileOutputStream outStream = null;
+	    		 try {
+	    			 String path = Environment.getExternalStorageDirectory() +
+	    					 "/test.jpg";
+	    			 outStream = new FileOutputStream(path);
+	    			 outStream.write(data);
+	    			 outStream.close();
+	    			 
+	    			 setContentView(R.layout.editor);
+	    			 Bitmap myBitmap = BitmapFactory.decodeFile(path);
+	    			 
+	    			 ImageView myImage = (ImageView) findViewById(R.id.imageView);
+	    			 myImage.setImageBitmap(myBitmap);
 
-	       rawCallback = new PictureCallback() {
-	        public void onPictureTaken(byte[] data, Camera camera) {
-	          // TODO Do something with the image RAW data.
-	        }
-	      };
-
-	       jpegCallback = new PictureCallback() {
-	        public void onPictureTaken(byte[] data, Camera camera) {
-	          // Save the image JPEG data to the SD card
-	          FileOutputStream outStream = null;
-	          try {outStream.close();
-
-	          } catch (FileNotFoundException e) {
-	            Log.e(tag, "File Note Found", e);
-	          } catch (IOException e) {
-	            Log.e(tag, "IO Exception", e);
-	          }
-	        }
-	        
-	      };
-	      
+	    			 
+	    		 } catch (FileNotFoundException e) {
+	    			 Log.e(tag, "File Note Found", e);
+	    		 } catch (IOException e) {
+	    			 Log.e(tag, "IO Exception", e);
+	    		 }
+	    	 }
+	     };
+	     postviewCallback = new PictureCallback() {
+	 		public void onPictureTaken(byte[] data, Camera camera) {			
+	 	        //sets theImage to the one of the selected path, and then sets the 
+	 	        //background Picture to that theImage
+	 	        Bitmap pictureTaken = BitmapFactory.decodeByteArray(data, 0, data.length);
+	 	        ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+	 	        editorPicture.setImageBitmap(pictureTaken);	        
+	 		}
+	 	};
+	 	//setContentView(R.layout.editor);
 	}
 
     private void takePicture() {
         camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-      }
+        SurfaceView mainscreenSurface = (SurfaceView) findViewById(R.id.surfaceView1);
+        mainscreenSurface.buildDrawingCache();
+        Bitmap pictureTaken = mainscreenSurface.getDrawingCache();
+	    ImageView editorPicture = (ImageView) findViewById(R.id.imageView);
+	    editorPicture.setImageBitmap(pictureTaken);	 
+        setContentView(R.layout.editor);
+    }
 	@Override
 	public void surfaceCreated(SurfaceHolder surfaceholder) {
 		try{
 		     camera = Camera.open();
-		     }catch(RuntimeException e){
-		     Log.e(tag, "init_camera: " + e);
-		     return;
-		     }
-		     Camera.Parameters param;
-		     param = camera.getParameters();
-		     //modify parameter
-		     param.setPreviewFrameRate(20);
-		     param.setPreviewSize(1280, 720);
-		     camera.setDisplayOrientation(270);
-		     camera.setParameters(param);
-		     try {
-		camera.setPreviewDisplay(surfaceHolder);
-		camera.startPreview();
-		} catch (Exception e) {
-		Log.e(tag, "init_camera: " + e);
-		return;
+		}catch(RuntimeException e){
+			Log.e(tag, "init_camera: " + e);
+			return;
 		}
-		  }
-	
+		Camera.Parameters param;
+		param = camera.getParameters();
+		//modify parameter
+		param.setPreviewFrameRate(20);
+		param.setPreviewSize(1280, 720);
+		camera.setDisplayOrientation(270);
+		camera.setParameters(param);
+		try {
+			camera.setPreviewDisplay(surfaceHolder);
+		    camera.startPreview();
+		} catch (Exception e) {
+			Log.e(tag, "init_camera: " + e);
+		    return;
+		}
+	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder surfaceholder) {
-		// TODO Auto-generated method stub
 		camera.stopPreview();
 		camera.release();
 		camera = null;
@@ -235,7 +287,7 @@ private final String tag = "mainscreen";
 public String getPath(Uri uri) {
     // just some safety built in 
     if( uri == null ) {
-        // TODO perform some logging or show user feedback
+        //perform some logging or show user feedback
         return null;
     }
     // try to retrieve the image from the media store first
@@ -250,5 +302,5 @@ public String getPath(Uri uri) {
     }
     // this is our fallback here
     return uri.getPath();
-}
+	}
 }
